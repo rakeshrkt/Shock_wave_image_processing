bc9bbda2114d76bfc16d6054aeddccd18c874631
Rakesh Kumar Thakur <86558469+rakeshrkt@users.noreply.github.com>
2023-10-17 02:23:39 +0530

    Update Shock.py

--- Shock.py
+++ Shock.py
@@ -1,68 +1,55 @@
 import cv2
 import numpy as np
-from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
-# Load the image
-image = cv2.imread(r'1920px-F4_p4_red_planedrop.jpg', cv2.IMREAD_GRAYSCALE)
+# image = cv2.imread(r'C:\Users\rakes\Downloads\bulletshock.jpg', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread(r'bulletshock.jpg', cv2.IMREAD_GRAYSCALE)
 
-# Resize the image to fit the screen dimensions 
-desired_width = 1920
-desired_height = 1080
+blurred = cv2.GaussianBlur(image, (47, 47), 0)
+edges = cv2.Canny(blurred, 50, 100)
 
-# Calculate the aspect ratio
-aspect_ratio = image.shape[1] / image.shape[0]
+#Plot the edges using matplotlib
+plt.imshow(edges, cmap='gray')
+plt.title('Canny Edges')
+plt.axis('off')
+plt.show()
 
-# Resize the image while maintaining the aspect ratio
-if aspect_ratio > 1:
-    new_width = desired_width
-    new_height = int(desired_width / aspect_ratio)
-else:
-    new_height = desired_height
-    new_width = int(desired_height * aspect_ratio)
-
-resized_image = cv2.resize(image, (new_width, new_height))
-
-
-
-# Apply Gaussian blur to reduce noise
-blurred = cv2.GaussianBlur(resized_image, (5, 5), 0)
-
-# Apply edge detection using the Canny method
-edges = cv2.Canny(blurred, 30, 150)
-
-# Find contours in the edge-detected image
 contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-all_points = []
 
+all_points = []
 # Loop through the detected contours and collect all points
 for contour in contours:
-    if len(contour) >= 2:
-        points = np.squeeze(contour, axis=1)  # Remove redundant dimensions
-        all_points.extend(points)
+    epsilon = 0.04 * cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, epsilon, True)
+    approx = np.squeeze(approx, axis=1)  # Remove unnecessary axis
+    # Add (x, y) values to the list
+    all_points.extend(approx.tolist())
 
-# Approximate the shock line as a polygon using cv2.approxPolyDP
-epsilon = 5  
-shock_polygon = cv2.approxPolyDP(np.array(all_points), epsilon, True)
+# Convert points to numpy arrays
+all_points = np.array(all_points)
+plt.figure()
+plt.imshow(edges, cmap= 'gray')
 
-# Draw the shock polygon on the resized image
-result = image.copy()
-cv2.polylines(result, [shock_polygon], isClosed=True, color=(0, 0, 255), thickness=2)
+# Define a function for linear regression (y = mx + b)
+def linear_func(x, m, b):
+    return m * x + b
 
-# Calculate the Mach number based on the slope of the approximated shock line
-line_params = cv2.fitLine(shock_polygon, cv2.DIST_L2, 0, 0.01, 0.01)
-slope = line_params[1] / line_params[0]
+# Fit a linear regression line using curve_fit
+x_data = all_points[:, 0]
+y_data = all_points[:, 1]
+params, _ = curve_fit(linear_func, x_data, y_data)
+slope, intercept = params
+y = slope*x_data + intercept
 
-mach_number = abs(1 / slope)  # Assuming a weak shock
+# Calculate the regression line points
+x_range = np.array([min(x_data), max(x_data)])
+y_range = linear_func(x_range, slope, intercept)
 
-print("Mach Number: %f" %mach_number)
+plt.plot(x_range, y_range, 'red')
+plt.axis('off')
+plt.show()
 
-# Display the result
-cv2.imshow('Shock Detection', result)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-
-
-
+# Mach number calculation using slope
+mach_number = abs(np.sqrt(slope**2+1)/slope)
+print('Mach Number: %f' %mach_number)
